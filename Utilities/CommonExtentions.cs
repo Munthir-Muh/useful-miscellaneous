2d11d4cc03e0ecffd372dd5b737f9f04fcbea514
Munthir <munthir.muh@gmail.com>
2023-05-30 20:22:01 +0300

    Add project files.

---
+++ Utilities/CommonExtentions.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace Utilities.Extentions
+{
+    public static class CommonExtentions
+    {
+        /// <summary>
+        /// Check if the underlaying object is Collection or not
+        /// </summary>
+        /// <param name="obj">the underlaying object</param>
+        /// <param name="ignoreString">optional parameter, if set to false string object will be considered as collection.Default value is true</param>
+        /// <returns>returns true if the object is collection otherwise returns false</returns>
+        public static bool IsCollection<T>(this T obj, bool ignoreString = true)
+        {
+           if(obj.GetType() == typeof(string) && !ignoreString)
+                  return true;
+
+           return (obj as IEnumerable) != null;
+        }
+        /// <summary>
+        /// Check if the underlaying Type is Collection or not
+        /// </summary>
+        /// <param name="type">the underlaying Type</param>
+        /// <param name="ignoreString">optional parameter, if set to false string type will be considered as collection.Default value is true</param>
+        /// <returns>returns true if the type is collection otherwise returns false</returns>
+        public static bool IsCollection(this Type type, bool ignoreString = true) =>
+            type.GetInterfaces().Any(s => s == typeof(IEnumerable)) && (ignoreString ? type != typeof(string) : !ignoreString);
+
+        /// <summary>
+        /// Try Get any public properties of the underlaying object type.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="target">the object we want to try to get its properties</param>
+        /// <param name="properties">out parameter of the properties that will be found</param>
+        /// <returns>returns false if the type is primitive or does not has public properties, otherwise returns true. </returns>
+        public static bool TryGetProperties<T>(this T target, out PropertyInfo[] properties)
+        {
+            var type = target.GetType();
+
+            properties = type.IsPrimitive || type == typeof(string) ?
+                null : type.GetProperties(BindingFlags.Public | BindingFlags.Instance);
+
+            return properties != null;
+        }
+        /// <summary>
+        /// Transform any object to IEnumerable Collection.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="obj">The object we need to Transform to collection</param>
+        /// <param name="isCollection">Out parameter will be true if the object is already a collection; otherwise, is false</param>
+        /// <param name="ignoreString">if set false, string will be considered as IEnumerable and will be returned as it is; otherwise, it will return IEnumerable<string>. the default value is true. </param>
+        /// <returns>return IEnum</returns>
+        public static IEnumerable ToEnumerable<T>(this T obj, out bool isCollection, bool ignoreString = true)
+        {
+            isCollection = false;
+
+            if (obj.IsCollection(ignoreString))
+            {
+                isCollection = true;
+                return obj as IEnumerable;
+            }
+
+            return Enumerable.Repeat(obj, 1);
+        }
+        /// <summary>
+        /// Convert any collection into a paginated collection that allows you to navigate through it as pages using methods such as
+        /// <see cref="PaginatedCollection{T}.GoToPageNo(int)"/>, <see cref="PaginatedCollection{T}.NextPage"/>, <see cref="PaginatedCollection{T}.PreviousPage"/>..etc
+        /// </summary>
+        public static PaginatedCollection<T> ToPaginatedCollection<T>(this IEnumerable<T> collection, int pageSize)
+        {
+            return new PaginatedCollection<T>(collection, pageSize);
+        }
+        /// <summary>
+        /// Return true if the underlying value exists within a list of values; otherwise, return false. This is similar to the SQL operator ‘IN’.
+        /// </summary>
+        public static bool InValues<T>(this T current, params T[] values)
+        {
+            if (values != null)
+            {
+                return values.Contains(current);
+            }
+
+            return false;
+        }
+        /// <summary>
+        /// Compare two values against target value and return true if the target value is between them; otherwise, return false.
+        /// </summary>
+        public static bool Between<T>(this T current, T leftSide, T rightSide) where T : IComparable<T>
+        {
+            return current.CompareTo(leftSide) >= 0 && current.CompareTo(rightSide) <= 0;
+        }
+
+        public static bool GreaterThan<T>(this T left, T right) where T : IComparable<T> =>
+            left.CompareTo(right) > 0;
+
+        public static bool LessThan<T>(this IComparable<T> left, T right) where T : IComparable<T> =>
+            left.CompareTo(right) < 0;
+        
+        public static bool Equal<T>(this T left, T right) where T : IComparable<T> => 
+            left.CompareTo(right) == 0;
+        public static bool LessThanOrEqual<T>(this T left, T right) where T : IComparable<T> =>
+            left.CompareTo(right) <= 0;
+
+        public static bool GreaterThanOrEqual<T>(this T left, T right) where T : IComparable<T> => 
+            left.CompareTo(right) >= 0;
+    }
+}
---
+++ Utilities/Structure/PaginatedCollection.cs
@@ -0,0 +1,71 @@
+﻿using System.Linq;
+
+namespace System.Collections.Generic
+{
+    public class PaginatedCollection<T> : IEnumerable<IEnumerable<T>>
+    {
+        private readonly IEnumerable<T> _internalCollection;
+        public int PageSize { get; private set; }
+        public int CurrentPageNo { get; private set; }
+        public int TotalPages => (int)Math.Ceiling((double)_internalCollection.Count() / PageSize);
+        public PaginatedCollection(IEnumerable<T> collection, int pageSize)
+        {
+            PageSize = pageSize;
+            _internalCollection = collection;
+        }
+        public IEnumerable<T> this[int pageNo] => GoToPageNo(pageNo);
+        public void ChangePageCapacity(int pagesize)
+        {
+            PageSize = pagesize;
+            CurrentPageNo = 1;
+        }
+        public IEnumerable<T> GoToPageNo(int pageNo)
+        {
+            CurrentPageNo = pageNo;
+
+            return _internalCollection.Skip(PageSize * (pageNo - 1))
+                .Take(PageSize);
+        }
+        public IEnumerable<T> NextPage()
+        {
+            return GoToPageNo(PageSize * CurrentPageNo++);
+        }
+        public IEnumerable<T> PreviousPage()
+        {
+            return GoToPageNo(PageSize * (CurrentPageNo--) - 2);
+        }
+        public IEnumerable<T> CurrentPage()
+        {
+            return GoToPageNo(PageSize * CurrentPageNo - 1);
+        }    
+        public IEnumerable<T> FirstPage()
+        {
+            return GoToPageNo(1);
+        }
+        public IEnumerable<T> LastPage()
+        {
+            return GoToPageNo(TotalPages);
+        }
+        public IEnumerable<T> GoToPageOfRecordNo(int recordNo)
+        {
+            var recortPageNo = Math.Ceiling((double)recordNo / PageSize);
+            return GoToPageNo(PageSize);
+        }
+        private IEnumerable<IEnumerable<T>> Browse()
+        {
+            for (int pages = 1; pages <= TotalPages; pages++)
+            {
+                yield return GoToPageNo(pages);
+            }
+        }
+        IEnumerator<IEnumerable<T>> IEnumerable<IEnumerable<T>>.GetEnumerator()
+        {
+            return Browse().GetEnumerator();
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            return Browse().GetEnumerator();
+        }
+    }
+}
